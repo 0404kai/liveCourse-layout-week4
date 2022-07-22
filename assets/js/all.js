30723855c91340b74d1236f0fc50387a34e1a8e3
Darren_Hsu <bthd0404@gmail.com>
2022-07-22 23:36:47 +0800

    Update 2022-07-22T15:36:34.664Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,22 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
+$('#burger').on('click', function () {
+  $('#burger').toggleClass('open');
+  $('#menu').slideToggle();
+});
+$('#optical').on('click', function () {
+  $('#sunglassesContent').fadeOut();
+  $('#sunglasses').parent('li').toggleClass('border-b-8 border-b-claret');
+  $('#opticalContent').fadeIn();
+  $('#optical').parent('li').toggleClass('border-b-8 border-b-claret');
+});
+$('#sunglasses').on('click', function () {
+  $('#opticalContent').fadeOut();
+  $('#optical').parent('li').toggleClass('border-b-8 border-b-claret');
+  $('#sunglassesContent').fadeIn();
+  $('#sunglasses').parent('li').toggleClass('border-b-8 border-b-claret');
+});
+$('#details').on('click', function () {
+  $('#area, #branch').fadeToggle('hidden');
 });
 //# sourceMappingURL=all.js.map
